e66b53c86c185bfb1de4fef88abb282ef1bd15ae
Hungry_munk <abbaszadeharsam@gmail.com>
2022-06-04 17:07:22 +1000

    added some basic page layout, like very basic and figured out ohw to add a book onto the page but no in a card form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const Main = document.querySelector('main')
+
 // declare empty library 
 let library = [];
 
@@ -13,4 +15,14 @@ function Book (Title, Author, Pages, Read) {
 function createBook (Title,Author,Pages,Read) {
     let newbook = new Book(Title,Author,Pages,Read)
     library.push(newbook)
+}
+
+createBook('gggg','sdfsdf',454,false)
+createBook('ggg','asd',454,false)
+createBook('gg','asdsad',567,true)
+
+for (let book of library){
+    const bookBox = document.createElement('div');
+    bookBox.textContent = `${book.Title}\n${book.Read}\n${book.Pages}\n${book.Author}`
+    Main.appendChild(bookBox)
 }
