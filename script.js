2de6c6c698368312185e6ec5ea0c9abdf3aa4cde
Hungry_munk <abbaszadeharsam@gmail.com>
2022-06-05 09:16:39 +1000

    Js can create card with all of stuff needed on it.

--- script.js
+++ script.js
@@ -24,16 +24,31 @@ function createBookElement (tag, text, className) {
     return BookElement
 }
 
+function createReadButton (read=false) {
+    const ReadButton = document.createElement('button')
+    ReadButton.classList.add('book-readStatus')
+    if (read) {
+        ReadButton.classList.add('read')
+        ReadButton.textContent = 'read'
+    } else {
+        ReadButton.classList.add('unread')
+        ReadButton.textContent = 'unread'
+    }
+    return ReadButton
+}
+
 createBook('gggg','sdfsdf',454,false)
 createBook('ggg','asd',454,false)
 createBook('gg','asdsad',567,true)
 
 for (const book of library){
     const bookItem = document.createElement('div');
-    
+    bookItem.classList.add('book-card')
     bookItem.appendChild(createBookElement('h2',book.Title,'book-title'));
     bookItem.appendChild(createBookElement('div',book.Author,'book-title'));
     bookItem.appendChild(createBookElement('div',book.Pages,'book-pages'));
+    bookItem.appendChild(createReadButton(book.Read))
+    bookItem.appendChild(createBookElement('button', 'remove','remove-btn'))
     
     Main.appendChild(bookItem)
 }
